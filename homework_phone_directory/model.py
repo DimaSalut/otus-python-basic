9b0fa3c943ab10ade07ea773011c5a508e6fb23a
danchik86nv <danchik86nv@yandex.ru>
2024-12-29 19:21:30 +0300

    commit for zergey

--- homework_phone_directory/model.py
+++ homework_phone_directory/model.py
@@ -1,6 +1,4 @@
 import json, os
-from dataclasses import dataclass
-
 
 path = os.path.join(os.path.dirname(__file__), 'contacts_100.json')
 
@@ -25,24 +23,36 @@ class Contact:
 
 
 class File:
-    # path = os.path.join(os.path.dirname(__file__), 'contacts_100.json')
-    @classmethod
-    def open_file(cls):
+    def open_file(self):
         if os.path.exists:
             with open(path, 'r', encoding='utf-8') as json_file:
                 data = json.load(json_file)
                 return data
 
-    @classmethod
-    def save_file(cls, saved_file):
+    @staticmethod
+    def save_file(saved_file):
         with open(path, 'w', encoding='utf-8') as json_file:
             json.dump(saved_file, json_file, ensure_ascii=True, indent = 4)
 
 
 class Dictionary:
-    pass
+    data = File.open_file()
 
-a = Contact(name='Jhon')
-b = Contact(name='Artem')
-print(a)
-print(b)
+    @classmethod
+    def show_contacts(cls):
+        for item in cls.data:
+            contact_obj = Contact(name=item['Имя'], second_name=item['Фамилия'], phone_number=item['Номер телефона'],
+                                  city=item['Город'])
+            print(contact_obj, '\n')
+
+    @classmethod
+    def create_contact(cls, name, second_name, phone_number, city):
+        return cls.data.append({'Имя': name, 'Фамилия': second_name, })
+
+
+
+
+
+
+
+
--- homework_phone_directory/view.py
+++ homework_phone_directory/view.py
@@ -1,1 +1,6 @@
+from controller import user_menu
+from homework_phone_directory.model import Dictionary
+
+if user_menu() == '1':
+    Dictionary.show_contacts()
 
